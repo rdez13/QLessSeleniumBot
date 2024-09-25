70cca51fe07410922d330a9a244d5a9b902ca589
rdez13 <ryandezfuli@yahoo.com>
2024-09-25 18:53:12 -0400

    QLess Selenium file added

---
+++ QLessSelenium.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+service = Service(executable_path="/Users/Rayamba/Desktop/pyautogui_QLess/chromedriver")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://kiosk.ca1.qless.com/kiosk/app/home/19713")
+time.sleep(5)
+
+# Check for the element with the specified class name
+try:
+    # Replace 'element_selector' with the actual selector for the element you want to check
+    element = driver.find_element(By.CLASS_NAME, "is-desktop")
+    # If found, skip the execution of the specific part
+    print("Element with class 'is-desktop' found.")
+    input_element = driver.find_element(By.ID, "consumerfield_firstName")
+    input_element.send_keys("Ryan")
+
+    input_element = driver.find_element(By.ID, "consumerfield_lastName")
+    input_element.send_keys("Dezfuli")
+
+    input_element = driver.find_element(By.ID, "consumerfield_phone")
+    input_element.send_keys("647-548-9734")
+
+    time.sleep(1)
+
+    input_element.send_keys(Keys.ENTER)
+
+
+        
+except Exception as e:
+        # If the element is not found, dont run the desired code
+        print("Element with class 'is-desktop' not found.")
+
+
+
+
+
+driver.quit
